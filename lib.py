9acdfcc111fb418edfdd6a9679a2307266d2195a
Babooch <oguh.sevla@protonmail.ch>
2022-04-25 11:14:53 +0200

    =)

--- lib.py
+++ lib.py
@@ -10,57 +10,63 @@ def scale(image, scale):
     return pygame.transform.scale(image, size)
 
 
-front = scale(pygame.image.load('assets/char_front.png'), 3)
-bottom = scale(pygame.image.load('assets/char_bottom.png'), 3)
-right = scale(pygame.image.load('assets/char_right.png'), 3)
-left = scale(pygame.image.load('assets/char_left.png'), 3)
+up = scale(pygame.image.load('assets/tank_up.png'), 2)
+down = scale(pygame.image.load('assets/tank_down.png'), 2)
+right = scale(pygame.image.load('assets/tank_right.png'), 2)
+left = scale(pygame.image.load('assets/tank_left.png'), 2)
+brick = scale(pygame.image.load('assets/brick.png'),2)
+bullet_left = scale(pygame.image.load('assets/bullet_0.png'),2)
+bullet_down = scale(pygame.image.load('assets/bullet_1.png'),2)
+bullet_right = scale(pygame.image.load('assets/bullet_2.png'),2)
+bullet_up = scale(pygame.image.load('assets/bullet_3.png'),2)
 
 
-def moves(p1, p2, keys):
+def moves(p1, p2, keys,bricks):
     horizontal = False
     if keys[pygame.K_z]:
-        p1.move("front")
+        p1.move("up",bricks)
         horizontal = True
     if keys[pygame.K_s]:
-        p1.move("bottom")
+        p1.move("down",bricks)
         horizontal = True
     if not horizontal:
         if keys[pygame.K_d]:
-            p1.move("right")
+            p1.move("right",bricks)
         if keys[pygame.K_q]:
-            p1.move("left")
+            p1.move("left",bricks)
 
     horizontal = False
     if keys[pygame.K_UP]:
-        p2.move("front")
+        p2.move("up",bricks)
         horizontal = True
     if keys[pygame.K_DOWN]:
-        p2.move("bottom")
+        p2.move("down",bricks)
         horizontal = True
     if not horizontal:
         if keys[pygame.K_RIGHT]:
-            p2.move("right")
+            p2.move("right",bricks)
         if keys[pygame.K_LEFT]:
-            p2.move("left")
+            p2.move("left",bricks)
 
-    if p1.x > 1600:
+    if p1.x > 576 :
+        p1.x = 576
+    elif p1.x < 0 :
         p1.x = 0
-    elif p1.x < 0:
-        p1.x = 1600
-    if p2.x > 1600:
+
+    if p2.x > 576 :
+        p2.x = 576
+    elif p2.x < 0 :
         p2.x = 0
-    elif p2.x < 0:
-        p2.x = 1600
 
-    if p1.y < 0:
-        p1.y = 800
-    elif p1.y > 800:
+    if p1.y > 576 :
+        p1.y = 576
+    elif p1.y < 0 :
         p1.y = 0
-    if p2.y < 0:
-        p2.y = 800
-    elif p2.y > 800:
-        p2.y = 0
 
+    if p2.y > 576 :
+        p2.y = 576
+    elif p2.y < 0 :
+        p2.y = 0
 
 class player:
     def __init__(self, x, y):
@@ -68,21 +74,46 @@ class player:
         self.y = y
         self.speed = 5
         self.hp = 3
-        self.cSprite = front
+        self.cSprite = up
 
-    def move(self, o):
+    def move(self, o,bricks):
         if o == "left":
+            for i in bricks:
+                if self.nRect("left").colliderect(i.rect()):
+                    self.cSprite = left
+                    return
             self.x = self.x - self.speed
             self.cSprite = left
         elif o == "right":
+            for i in bricks:
+                if self.nRect("right").colliderect(i.rect()):
+                    self.cSprite = right
+                    return
             self.x = self.x + self.speed
             self.cSprite = right
-        elif o == "bottom":
+        elif o == "down":
+            for i in bricks:
+                if self.nRect("down").colliderect(i.rect()):
+                    self.cSprite = down
+                    return
             self.y = self.y + self.speed
-            self.cSprite = bottom
+            self.cSprite = down
         else:
+            for i in bricks:
+                if self.nRect("up").colliderect(i.rect()):
+                    self.cSprite = up
+                    return
             self.y = self.y - self.speed
-            self.cSprite = front
+            self.cSprite = up
+
+    def orientation(self):
+        if self.cSprite == up:
+            return "up"
+        elif self.cSprite == down:
+            return "down"
+        elif self.cSprite == right:
+            return "right"
+        return "left"
 
     def sprite(self):
         return self.cSprite
@@ -91,15 +122,64 @@ class player:
         return (self.x, self.y)
 
     def rect(self):
-        if self.cSprite == front or self.cSprite == bottom:
-            return pygame.Rect((self.x, self.y), (54, 72))
-        return pygame.Rect((self.x, self.y), (72, 54))
+        return pygame.Rect((self.x+8, self.y+8), (48, 48))
 
     def nRect(self, o):
-        if o == "front":
-            return pygame.Rect((self.x, self.y - self.speed), (54, 72))
-        if o == "bottom":
-            return pygame.Rect((self.x, self.y + self.speed), (54, 72))
+        if o == "up":
+            return pygame.Rect((self.x+8, self.y+8 - self.speed), (48, 48))
+        if o == "down":
+            return pygame.Rect((self.x+8, self.y+8 + self.speed), (48, 48))
         if o == "right":
-            return pygame.Rect((self.x + self.speed, self.y), (72, 54))
-        return pygame.Rect((self.x - self.speed, self.y), (72, 54))
+            return pygame.Rect((self.x+8 + self.speed, self.y+8), (48, 48))
+        return pygame.Rect((self.x +8 - self.speed, self.y+8), (48, 48))
+
+class newBrick:
+    def __init__(self, x, y):
+        self.x = x*64
+        self.y = y*64
+        self.cSprite = brick
+
+    def sprite(self):
+        return self.cSprite
+
+    def rect(self):
+        return pygame.Rect((self.x, self.y), (64, 64))
+
+    def coords(self):
+        return (self.x,self.y)
+
+class newBullet:
+    def __init__(self, x, y,o,sender):
+        self.x = x
+        self.y = y
+        self.speed = 8
+        self.o = o
+        self.sender = sender
+        if self.o == "up":
+            self.cSprite = bullet_up
+        elif self.o == "down":
+            self.cSprite = bullet_down
+        elif self.o == "right":
+            self.cSprite = bullet_right
+        else:
+            self.cSprite = bullet_left
+    def sprite(self):
+        return self.cSprite
+
+    def rect(self):
+        return pygame.Rect((self.x, self.y), (16, 16))
+
+    def coords(self,players,bricks):
+        if self.o == "right":
+            self.x = self.x + self.speed
+        elif self.o == "left":
+            self.x = self.x - self.speed
+        elif self.o == "up":
+            self.y = self.y - self.speed
+        else:
+            self.y = self.y + self.speed
+
+
+        return (self.x,self.y)
+
+
--- main.py
+++ main.py
@@ -2,25 +2,54 @@ import pygame
 import lib
 
 pygame.init()
-surface = pygame.display.set_mode((1600,800))
+surface = pygame.display.set_mode((640,640))
 pygame.display.set_caption("Battle City")
 pygame.display.set_icon(pygame.image.load('assets/icon.png'))
 
 clock = pygame.time.Clock()
 
-p1 = lib.player(400, 400)
-p2 = lib.player(1200, 400)
+bricks = []
+for i in range(0,10):
+    bricks.append(lib.newBrick(2, i))
+    bricks.append(lib.newBrick(7, i))
+for i in range(0,10):
+    bricks.append(lib.newBrick(i, 2))
+    bricks.append(lib.newBrick(i, 7))
+p1 = lib.player(0, 0)
+p2 = lib.player(576, 576)
+players = [p1,p2]
+bullets = [lib.newBullet(250, 250,"up",p1)]
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bullets.append(lib.newBullet(p1.x + 16, p1.y + 16, p1.orientation(), p1))
     keys = pygame.key.get_pressed()
-    lib.moves(p1,p2,keys)
-    pygame.draw.rect(surface, (0,0,0), pygame.Rect((0, 0), (1600, 800)))
-    pygame.draw.rect(surface, (255,0,0), p1.rect())
-    surface.blit(p1.sprite(),p1.coords())
-    pygame.draw.rect(surface, (255, 0, 0), p2.rect())
+    lib.moves(p1,p2,keys,bricks)
+    pygame.draw.rect(surface, (0,0,0), pygame.Rect((0, 0), (960, 960)))
+    surface.blit(p1.sprite(), p1.coords())
     surface.blit(p2.sprite(), p2.coords())
+    for i in bricks:
+        surface.blit(i.sprite(), i.coords())
+    for i in bullets:
+        surface.blit(i.sprite(), i.coords(players,bricks))
+    for i in bullets:
+        bullet = i
+        for b in bricks:
+            if i.rect().colliderect(b.rect()):
+                bullets.remove(i)
+                bricks.remove(b)
+                break
+        for p in players:
+            if i.rect().colliderect(p.rect()):
+                if not p == i.sender:
+                    bullets.remove(i)
+                    p.hp = p.hp - 1
+                    print(p.hp)
+                    break
+
     pygame.display.flip()
     clock.tick(120)
